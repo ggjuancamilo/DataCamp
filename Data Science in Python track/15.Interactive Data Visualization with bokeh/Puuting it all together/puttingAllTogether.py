746d41e04669d43bac74475c45761ded6e057395
Juan Camilo Gallego Gallon <jc.gallego10@uniandes.edu.co>
2020-05-12 17:18:41 -0500

    Some courses in DataScienceTrack

---
+++ Data Science in Python track/15.Interactive Data Visualization with bokeh/Puuting it all together/puttingAllTogether.py
@@ -0,0 +1,101 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from bokeh.io import output_file, show, curdoc
+from bokeh.plotting import figure
+from bokeh.models import ColumnDataSource, CategoricalColorMapper, HoverTool, Slider, Column, Select, Button, CategoricalColorMapper
+from bokeh.layouts import row, column, widgetbox
+from bokeh.palettes import Spectral6
+
+data = pd.read_csv('gapminder_tidy.csv', index_col='Year')
+
+# Make a list of the unique values from the region column: regions_list
+regions_list = data.region.unique().tolist()
+
+# Make a color mapper: color_mapper
+color_mapper = CategoricalColorMapper(factors=regions_list, palette=Spectral6)
+
+# Make the ColumnDataSource: source
+source = ColumnDataSource(data={
+    'x'       : data.loc[1970].fertility,
+    'y'       : data.loc[1970].life,
+    'country'      : data.loc[1970].Country,
+    'pop'      : (data.loc[1970].population / 20000000) + 2,
+    'region'      : data.loc[1970].region,
+})
+
+# Create the figure: plot
+plot = figure(title='Gapminder Data for 1970', plot_height=400, plot_width=700)
+
+# Add the color mapper to the circle glyph
+plot.circle(x='x', y='y', fill_alpha=0.8, source=source,
+            color=dict(field='region', transform=color_mapper), legend_group='region')
+
+# Set the legend.location attribute of the plot to 'top_right'
+plot.legend.location = 'bottom_left'
+
+
+# Define the callback: update_plot
+def update_plot(attr, old, new):
+    # Read the current value off the slider and 2 dropdowns: yr, x, y
+    yr = slider.value
+    x = x_select.value
+    y = y_select.value
+    # Label axes of plot
+    plot.xaxis.axis_label = x
+    plot.yaxis.axis_label = y
+    # Set new_data
+    new_data = {
+        'x'       : data.loc[yr][x],
+        'y'       : data.loc[yr][y],
+        'country' : data.loc[yr].Country,
+        'pop'     : (data.loc[yr].population / 20000000) + 2,
+        'region'  : data.loc[yr].region,
+    }
+    # Assign new_data to source.data
+    source.data = new_data
+
+    # Set the range of all axes
+    plot.x_range.start = min(data[x])
+    plot.x_range.end = max(data[x])
+    plot.y_range.start = min(data[y])
+    plot.y_range.end = max(data[y])
+
+    # Add title to plot
+    plot.title.text = 'Gapminder data for %d' % yr
+
+# Create a dropdown slider widget: slider
+slider = Slider(start=1970, end=2010, step=1, value=1970, title='Year')
+
+# Attach the callback to the 'value' property of slider
+slider.on_change('value', update_plot)
+
+# Create a dropdown Select widget for the x data: x_select
+x_select = Select(
+    options=['fertility', 'life', 'child_mortality', 'gdp'],
+    value='fertility',
+    title='x-axis data'
+)
+
+# Attach the update_plot callback to the 'value' property of x_select
+x_select.on_change('value', update_plot)
+
+# Create a dropdown Select widget for the y data: y_select
+y_select = Select(
+    options=['fertility', 'life', 'child_mortality', 'gdp'],
+    value='life',
+    title='y-axis data'
+)
+
+# Attach the update_plot callback to the 'value' property of y_select
+y_select.on_change('value',update_plot)
+
+hover = HoverTool(tooltips=[('Country', '@country')])
+plot.add_tools(hover)
+
+# Create layout and add to current document
+layout = row(Column(slider, x_select, y_select), plot)
+curdoc().add_root(layout)
+curdoc().title = 'Gapminder'
+output_file('plot.html')
